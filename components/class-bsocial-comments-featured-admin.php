aeb77e69f2a530765e972046de5cb6eb1cc38dd3
Stephen Page <stephenjpage@gmail.com>
2014-09-11 17:32:02 -0400

    Remove the feature link from the trash and spam pages

--- components/class-bsocial-comments-featured-admin.php
+++ components/class-bsocial-comments-featured-admin.php
@@ -57,9 +57,12 @@ class bSocial_Comments_Featured_Admin extends bSocial_Comments_Featured
 		{
 			return $actions;
 		}
-
-		// Get feature/unfeature link for the comment
-		$actions['feature-comment'] = $this->get_feature_link( $comment->comment_ID );
+		global $comment_status;
+		if ( 'trash' != $comment_status && 'spam' != $comment_status )
+		{
+			// Get feature/unfeature link for the comment
+			$actions['feature-comment'] = $this->get_feature_link( $comment->comment_ID );
+		}
 
 		return $actions;
 	} // END comment_row_actions
