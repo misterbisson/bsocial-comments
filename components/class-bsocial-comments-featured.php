945b11e29fc6ba7638fd3877a30f805415b3a9a9
Jamie Poitra <jamie@gigaom.com>
2014-09-10 17:46:44 -0700

    Added current_user_can( 'edit_comment', $comment_id ) to ajax moderation functions

--- components/class-bsocial-comments-featured.php
+++ components/class-bsocial-comments-featured.php
@@ -472,8 +472,9 @@ class bSocial_Comments_Featured
 	{
 		$comment_id = absint( $_GET['comment_id'] );
 
-		if ( ! current_user_can( 'moderate_comments' ) )
+		if ( ! current_user_can( 'moderate_comments' ) && ! current_user_can( 'edit_comment', $comment_id ) )
 		{
+			print_r('stop!'); exit();
 			return FALSE;
 		}//END if
 
--- components/class-bsocial-comments.php
+++ components/class-bsocial-comments.php
@@ -381,7 +381,7 @@ class bSocial_Comments
 		$comment_id = absint( $_GET['comment_id'] );
 		$direction  = $_GET['direction'];
 
-		if ( ! current_user_can( 'moderate_comments' ) )
+		if ( ! current_user_can( 'moderate_comments' ) && ! current_user_can( 'edit_comment', $comment_id ) )
 		{
 			return wp_send_json_error();
 		} // END if
